583850e118f2e0004c24e177b66907b610df5f9c
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2023-05-12 17:28:18 +0200

    stuff

--- src/server.lua
+++ src/server.lua
@@ -1,6 +1,17 @@
 require("shared.communication")
 
 
+-- MESSAGE HANDLERS --
+
+
+MessageHandler = {
+    [Communication.messages.getProject]
+}
+
+
+
+-- MAIN --
+
 
 if fs.exists("state") then
     local fileHandle = fs.open("state", "r")
@@ -15,5 +26,6 @@ end
 
 while true do
     local id, msg = rednet.receive(Communication.protocol)
-    
+    local payload = textutils.unserialize(msg)
+
 end
